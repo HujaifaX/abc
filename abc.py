1a0cd429fa098bab894f149a5448f9f2e464c66c
HujaifaX <97424865+HujaifaX@users.noreply.github.com>
2022-05-05 12:47:38 +0600

    abc.py
    git clone https://github.com/HujaifaX/abc.git
    ls
    cd abc
    python abc.py

--- abc.py
+++ abc.py
@@ -2,10 +2,10 @@ import os, platform
 bit = platform.architecture()[0]
 def __init__():
     if bit == '64bit':
-        from mao_main import defultchk
+        from abc_main import defultchk
         defultchk()
     elif bit == '32bit':
-        from mao_main_32 import defultchk
+        from abc_main_32 import defultchk
         defultchk()
     else:
         print(bit)
